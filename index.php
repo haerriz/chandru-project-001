3b42dd4b8f8dc1a031c7b40e639f96a8c9bcea9b
Haerriz <haerriz@gmail.com>
2020-03-09 00:24:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Final Project</title>
+    <style>
+      .one,.two{
+        display:block;
+        width:50%;
+        text-align:center;
+        color: red;
+        text-decoration:none;
+      }
+      h1{
+        text-align: center;
+      }
+    </style>
   </head>
   <body>
-    <a href="student.php">student</a>
-    <a href="sponsor.php">sponsor</a>
-
+    <h1>Welcome</h1>
+    <a class="one" href="student.php">student</a>
+    <a class="two" href="sponsor.php">sponsor</a>
 </html>
